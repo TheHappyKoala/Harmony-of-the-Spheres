0d58051eff83eeb68f19b6e949fa6022de2ee38b
Darrell Huffman <rather.glorious@gmail.com>
2018-06-08 10:25:40 +0200

    Add array of prominent celestial bodies

---
+++ src/js/data/masses/index.js
@@ -0,0 +1,14 @@
+export default [
+  { m: 3.003e-6, name: 'Earth' },
+  { m: 3.213e-7, name: 'Mars' },
+  { m: 9.543e-4, name: 'Jupiter' },
+  { m: 3.69396868e-8, name: 'Moon' },
+  { m: 6.58086572e-9, name: 'Pluto' },
+  { m: 0.0002857, name: 'Saturn' },
+  { m: 0.00005149, name: 'Neptune' },
+  { m: 7.450777301989e-8, name: 'Ganymede' },
+  { m: 0.00004365, name: 'Uranus' },
+  { m: 0.08, name: 'Hanky The Brown Dwarf' },
+  { m: 0.1, name: 'Proxima Centauri' },
+  { m: 1, name: 'Sun' }
+];
