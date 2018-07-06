49046bdec9198840d1658208641ef97b7ba8e136
Darrell Huffman <rather.glorious@gmail.com>
2018-07-06 21:51:30 +0200

    Add collision explosions
    A cheap animation that is rendered when a collision has taken place. Needs improvement, but it's a start.

--- src/js/scene/MassManifestation.js
+++ src/js/scene/MassManifestation.js
@@ -3,6 +3,7 @@ import * as THREE from 'three';
 export default class extends THREE.Object3D {
   constructor(mass) {
     super();
+
     this.mass = mass;
     this.name = mass !== undefined ? mass.name : 'Decorative Sun';
     this.textureLoader = new THREE.TextureLoader();
@@ -60,9 +61,44 @@ export default class extends THREE.Object3D {
     this.getMain();
   }
 
+  createExplosion() {
+    const explosionGeometry = new THREE.Geometry();
+
+    const mainPosition = this.getObjectByName('Main').position;
+
+    const velocity = this.mass.radius;
+
+    for (let i = 0; i < 1000; i++) {
+      let particle = new THREE.Vector3().copy(mainPosition);
+
+      particle.v = new THREE.Vector3(
+        Math.random() * velocity - velocity / 2,
+        Math.random() * velocity - velocity / 2,
+        Math.random() * velocity - velocity / 2
+      );
+
+      explosionGeometry.vertices.push(particle);
+    }
+
+    const particleMaterial = new THREE.PointsMaterial({
+      size: 0.1,
+      map: this.textureLoader.load('./textures/fragment.png'),
+      transparent: true,
+      opacity: 0.8
+    });
+
+    const explosion = new THREE.Points(explosionGeometry, particleMaterial);
+    explosion.name = 'Explosion';
+    explosion.userData.iteration = 0;
+    explosion.userData.ticks = 50;
+
+    this.add(explosion);
+  }
+
   draw(x, y, z) {
     const main = this.getObjectByName('Main');
     const trail = this.getObjectByName('Trail');
+    const explosion = this.getObjectByName('Explosion');
 
     main.position.set(x, y, z);
 
@@ -70,6 +106,24 @@ export default class extends THREE.Object3D {
       trail.geometry.vertices.unshift(new THREE.Vector3().copy(main.position));
       trail.geometry.vertices.length = this.mass.trailVertices;
       trail.geometry.verticesNeedUpdate = true;
+    }
+
+    if (explosion !== undefined) {
+      explosion.userData.iteration++;
+
+      if (explosion.userData.iteration < explosion.userData.ticks) {
+        for (let i = 0; i < explosion.geometry.vertices.length; i++) {
+          let explosionFragment = explosion.geometry.vertices[i];
+
+          explosionFragment.x += explosionFragment.v.x;
+          explosionFragment.y += explosionFragment.v.y;
+          explosionFragment.z += explosionFragment.v.z;
+
+          //explosionFragment.v.y -= 0.5 / 10;
+        }
+      } else this.remove(explosion);
+
+      explosion.geometry.verticesNeedUpdate = true;
     }
   }
 }
