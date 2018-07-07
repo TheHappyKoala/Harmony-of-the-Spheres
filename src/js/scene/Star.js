6dc41bb03cc60a0aefea2daa0d5475976712c84d
Darrell Huffman <rather.glorious@gmail.com>
2018-07-07 21:14:50 +0200

    Create a fancier star model

--- src/js/scene/Star.js
+++ src/js/scene/Star.js
@@ -7,25 +7,37 @@ export default class extends MassManifestation {
   }
 
   getMain() {
-    const texture = this.textureLoader.load('textures/sun.png');
+    const container = new THREE.Object3D();
+    container.name = 'Main';
 
-    const sprite = new THREE.Sprite(
+    const texture = this.textureLoader.load('textures/corona.png');
+
+    const corona = new THREE.Sprite(
       new THREE.SpriteMaterial({
         map: texture,
         blending: THREE.AdditiveBlending,
         color: 0xffffff
       })
     );
-    sprite.name = 'Main';
-
-    const radius = this.mass !== undefined ? this.mass.radius : 7;
-
-    sprite.scale.set(radius, radius, radius);
+    corona.scale.set(3.5, 3.5, 3.5);
 
     const light = new THREE.PointLight(0xffffff, 3, 310);
     light.position.set(0, 0, 0);
     light.color.setHSL(0.55, 0.1, 0.5);
 
-    this.add(sprite, light);
+    const sphere = new THREE.SphereGeometry(
+      this.mass !== undefined ? this.mass.radius : 0.5,
+      32,
+      32
+    );
+
+    const sphereMaterial = new THREE.MeshBasicMaterial({ color: 'white' });
+
+    const sphereMesh = new THREE.Mesh(sphere, sphereMaterial);
+    sphereMesh.rotation.x = Math.PI / 2;
+
+    container.add(sphereMesh, corona, light);
+
+    this.add(container);
   }
 }
