2061efec2d95ae6339dd557972c865ab1fdf3bb2
Darrell <rather.glorious@gmail.com>
2018-12-06 16:27:12 +0100

    Fix stupid syntax typo in the getRandom color function

--- src/js/utils.js
+++ src/js/utils.js
@@ -1,6 +1,6 @@
 export function getRandomColor() {
-  let letters = '0123456789ABCDEF';
-  const color = '#';
+  const letters = '0123456789ABCDEF';
+  let color = '#';
 
   for (let i = 0; i < 6; i++) color += letters[Math.floor(Math.random() * 16)];
 
