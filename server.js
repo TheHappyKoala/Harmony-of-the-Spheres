32dc18ae27deec0a7a63857dcca4e42fea3b64ac
Darrell <rather.glorious@gmail.com>
2018-12-28 10:11:03 +0100

    No need for the static express server
    We serve the app on GitHub and not Heroku, for the time being, so no need to keep the static file server.

--- server.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const app = express();
-
-app.use(express.static(`${__dirname}/dist/`));
-
-const port = process.env.PORT;
-
-app.listen(port, () => console.log(`Listening to ${port}`));
