10d5ca397debd425758918c20b30b6ee0cd2e59b
Darrell <rather.glorious@gmail.com>
2019-01-14 20:42:59 +0100

    Awesome stuff... Set actual colors for stars
    Found this website where color values for various types of stars and well known stars are listed, so now we can give our stars realistic colors! Here's the website in question: http://www.vendian.org/mncharity/dir3/starcolor/

--- src/js/scene/Star.js
+++ src/js/scene/Star.js
@@ -2,8 +2,8 @@ import * as THREE from 'three';
 import MassManifestation from './MassManifestation';
 
 export default class extends MassManifestation {
-  constructor(mass, manager) {
-    super(mass, manager);
+  constructor(mass) {
+    super(mass);
   }
 
   getMain() {
@@ -15,16 +15,15 @@ export default class extends MassManifestation {
     const corona = new THREE.Sprite(
       new THREE.SpriteMaterial({
         map: texture,
-        blending: THREE.AdditiveBlending,
-        color: 0xffffff
+        color: this.mass.color
       })
     );
 
-    const scaledRadius = this.mass.radius * 10;
+    const scaledRadius = this.mass.radius * 7;
 
     corona.scale.set(scaledRadius, scaledRadius, scaledRadius);
 
-    if (this.mass.light !== false) {
+    if (this.light !== false) {
       const light = new THREE.PointLight(0xffffff, 2.7, 0);
       light.position.set(0, 0, 0);
       light.color.setHSL(0.55, 0.1, 0.5);
@@ -32,9 +31,11 @@ export default class extends MassManifestation {
       container.add(light);
     }
 
-    const geometry = new THREE.SphereBufferGeometry(this.mass.radius, 32, 32);    
+    const geometry = new THREE.SphereBufferGeometry(this.mass.radius, 32, 32);
 
-    const material = new THREE.MeshBasicMaterial({ color: 'white' });
+    const material = new THREE.MeshBasicMaterial({
+      color: this.mass.color
+    });
 
     const mesh = new THREE.Mesh(geometry, material);
     mesh.rotation.x = Math.PI / 2;
