bc573a4c410d19b802b132bfc69598e71e058112
Darrell <rather.glorious@gmail.com>
2019-02-10 23:26:00 +0100

    Add component for lazy loading images

---
+++ src/js/components/LazyDog/index.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import './LazyDog.less';
+
+export default class extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      imageLoading: true
+    };
+  }
+
+  handleOnLoad = () => this.setState({ imageLoading: false });
+
+  render() {
+    return (
+      <figure className="lazy-dog-wrapper">
+        <img
+          onLoad={this.handleOnLoad}
+          src={this.props.src}
+          alt={this.props.alt}
+          width={this.props.width}
+          height={this.props.height}
+          className={!this.state.imageLoading && 'loaded-image'}
+        />
+        <figcaption>{this.props.caption}</figcaption>
+        {this.state.imageLoading && (
+          <i className={this.props.placeHolderIcon} />
+        )}
+      </figure>
+    ); 
+  }
+}
