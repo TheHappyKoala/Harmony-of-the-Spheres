0395fff995c786542a10f4c7d01ad0c8194d3694
Darrell <rather.glorious@gmail.com>
2019-04-20 23:25:26 +0200

    Refactor Camera component

--- src/js/components/Content/Camera.js
+++ src/js/components/Content/Camera.js
@@ -2,145 +2,143 @@ import React, { Fragment } from 'react';
 import Dropdown from '../Dropdown';
 import Tooltip from '../Tooltip';
 
-export default function(props) {
-  return (
-    <Fragment>
-      <h2>Camera</h2>
-      <label className="top">
-        Rotating Reference Frame{' '}
-        <Tooltip
-          position="left"
-          content="Specifying a rotating reference frames allows us to observe the universe unfold relative to a fixed point, for instance Earth. While Earth orbits the Sun regardless of the reference frame being considered, in a rotating reference frame, the sun, for example, will appear to orbit the Earth, which is fixed at the center of the coordinate system."
-        />
-      </label>
-      <div className="tabs-dropdown-wrapper">
-        <Dropdown selectedOption={props.rotatingReferenceFrame}>
+export default props => (
+  <Fragment>
+    <h2>Camera</h2>
+    <label className="top">
+      Rotating Reference Frame{' '}
+      <Tooltip
+        position="left"
+        content="Specifying a rotating reference frames allows us to observe the universe unfold relative to a fixed point, for instance Earth. While Earth orbits the Sun regardless of the reference frame being considered, in a rotating reference frame, the sun, for example, will appear to orbit the Earth, which is fixed at the center of the coordinate system."
+      />
+    </label>
+    <div className="tabs-dropdown-wrapper">
+      <Dropdown selectedOption={props.rotatingReferenceFrame}>
+        <div
+          name="Origo"
+          key="Origo"
+          callback={() =>
+            props.modifyScenarioProperty({
+              key: 'rotatingReferenceFrame',
+              value: 'Origo'
+            })
+          }
+        >
+          Origo
+        </div>
+        <div
+          name="Barycenter"
+          key="Barycenter"
+          callback={() =>
+            props.modifyScenarioProperty({
+              key: 'rotatingReferenceFrame',
+              value: 'Barycenter'
+            })
+          }
+        >
+          Barycenter
+        </div>
+        {props.masses.map(mass => (
           <div
-            name="Origo"
-            key="Origo"
+            name={mass.name}
+            key={mass.name}
             callback={() =>
               props.modifyScenarioProperty({
                 key: 'rotatingReferenceFrame',
-                value: 'Origo'
+                value: mass.name
               })
             }
           >
-            Origo
+            {mass.name}
           </div>
+        ))}
+      </Dropdown>
+    </div>
+    <label className="top">
+      Camera Position{' '}
+      <Tooltip
+        position="left"
+        content="Select the position of the camera. If the position is set to free, you can zoom in on and orbit around the focus of the camera with your mouse or touch screen."
+      />
+    </label>
+    <div className="tabs-dropdown-wrapper">
+      <Dropdown selectedOption={props.cameraPosition}>
+        <div
+          name="Free"
+          key="Free"
+          callback={() =>
+            props.modifyScenarioProperty({
+              key: 'cameraPosition',
+              value: 'Free'
+            })
+          }
+        >
+          Free
+        </div>
+        {props.masses.map(mass => (
           <div
-            name="Barycenter"
-            key="Barycenter"
-            callback={() =>
-              props.modifyScenarioProperty({
-                key: 'rotatingReferenceFrame',
-                value: 'Barycenter'
-              })
-            }
-          >
-            Barycenter
-          </div>
-          {props.masses.map(mass => (
-            <div
-              name={mass.name}
-              key={mass.name}
-              callback={() =>
-                props.modifyScenarioProperty({
-                  key: 'rotatingReferenceFrame',
-                  value: mass.name
-                })
-              }
-            >
-              {mass.name}
-            </div>
-          ))}
-        </Dropdown>
-      </div>
-      <label className="top">
-        Camera Position{' '}
-        <Tooltip
-          position="left"
-          content="Select the position of the camera. If the position is set to free, you can zoom in on and orbit around the focus of the camera with your mouse or touch screen."
-        />
-      </label>
-      <div className="tabs-dropdown-wrapper">
-        <Dropdown selectedOption={props.cameraPosition}>
-          <div
-            name="Free"
-            key="Free"
+            name={mass.name}
+            key={mass.name}
             callback={() =>
               props.modifyScenarioProperty({
                 key: 'cameraPosition',
-                value: 'Free'
+                value: mass.name
               })
             }
           >
-            Free
+            {mass.name}
           </div>
-          {props.masses.map(mass => (
-            <div
-              name={mass.name}
-              key={mass.name}
-              callback={() =>
-                props.modifyScenarioProperty({
-                  key: 'cameraPosition',
-                  value: mass.name
-                })
-              }
-            >
-              {mass.name}
-            </div>
-          ))}
-        </Dropdown>
-      </div>
-      <label className="top">
-        Camera Focus{' '}
-        <Tooltip
-          position="left"
-          content="Select the focus of the camera, or in other words, what the camera should be looking at."
-        />
-      </label>
-      <div className="tabs-dropdown-wrapper">
-        <Dropdown selectedOption={props.cameraFocus}>
+        ))}
+      </Dropdown>
+    </div>
+    <label className="top">
+      Camera Focus{' '}
+      <Tooltip
+        position="left"
+        content="Select the focus of the camera, or in other words, what the camera should be looking at."
+      />
+    </label>
+    <div className="tabs-dropdown-wrapper">
+      <Dropdown selectedOption={props.cameraFocus}>
+        <div
+          name="Origo"
+          key="Origo"
+          callback={() =>
+            props.modifyScenarioProperty({
+              key: 'cameraFocus',
+              value: 'Origo'
+            })
+          }
+        >
+          Origo
+        </div>
+        <div
+          name="Barycenter"
+          key="Barycenter"
+          callback={() =>
+            props.modifyScenarioProperty({
+              key: 'cameraFocus',
+              value: 'Barycenter'
+            })
+          }
+        >
+          Barycenter
+        </div>
+        {props.masses.map(mass => (
           <div
-            name="Origo"
-            key="Origo"
+            name={mass.name}
+            key={mass.name}
             callback={() =>
               props.modifyScenarioProperty({
                 key: 'cameraFocus',
-                value: 'Origo'
+                value: mass.name
               })
             }
           >
-            Origo
+            {mass.name}
           </div>
-          <div
-            name="Barycenter"
-            key="Barycenter"
-            callback={() =>
-              props.modifyScenarioProperty({
-                key: 'cameraFocus',
-                value: 'Barycenter'
-              })
-            }
-          >
-            Barycenter
-          </div>
-          {props.masses.map(mass => (
-            <div
-              name={mass.name}
-              key={mass.name}
-              callback={() =>
-                props.modifyScenarioProperty({
-                  key: 'cameraFocus',
-                  value: mass.name
-                })
-              }
-            >
-              {mass.name}
-            </div>
-          ))}
-        </Dropdown>
-      </div>
-    </Fragment>
-  );
-}
+        ))}
+      </Dropdown>
+    </div>
+  </Fragment>
+);
