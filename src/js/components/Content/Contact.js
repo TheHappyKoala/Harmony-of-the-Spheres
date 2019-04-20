0291df5e639787eabae4b797ac25dd1688177bf0
Darrell <rather.glorious@gmail.com>
2019-04-20 23:26:41 +0200

    Refactor Contact component

--- src/js/components/Content/Contact.js
+++ src/js/components/Content/Contact.js
@@ -1,20 +1,18 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 
-export default function() {
-  return (
-    <React.Fragment>
-      <h1>Contact</h1>
-      <p className="modal-item">
-        Want to say hi? Is there a scenario that you could not find but would
-        like to see included? Got feedback on how I could make Harmony of the
-        Spheres more user friendly or would you like to contribute to its
-        development? Then I suggest you either shoot me an email or hit me up on
-        Twitter! You will find my contact information below.
-      </p>
-      <h3 className="modal-item">Email</h3>
-      <p className="modal-item">darrell.arjuna.huffman@gmail.com</p>
-      <h3 className="modal-item">Twitter</h3>
-      <p className="modal-item">@DarrellAHuffma1</p>
-    </React.Fragment>
-  );
-}
+export default () => (
+  <Fragment>
+    <h1>Contact</h1>
+    <p className="modal-item">
+      Want to say hi? Is there a scenario that you could not find but would like
+      to see included? Got feedback on how I could make Harmony of the Spheres
+      more user friendly or would you like to contribute to its development?
+      Then I suggest you either shoot me an email or hit me up on Twitter! You
+      will find my contact information below.
+    </p>
+    <h3 className="modal-item">Email</h3>
+    <p className="modal-item">darrell.arjuna.huffman@gmail.com</p>
+    <h3 className="modal-item">Twitter</h3>
+    <p className="modal-item">@DarrellAHuffma1</p>
+  </Fragment>
+);
