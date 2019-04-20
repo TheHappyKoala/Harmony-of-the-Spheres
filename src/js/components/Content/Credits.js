91e6d7446e53c50772c6774ad58a50bf0036f055
Darrell <rather.glorious@gmail.com>
2019-04-20 23:27:35 +0200

    Refactor Credits component

--- src/js/components/Content/Credits.js
+++ src/js/components/Content/Credits.js
@@ -1,113 +1,111 @@
 import React, { Fragment } from 'react';
 
-export default function() {
-  return (
-    <Fragment>
-      <h1>Credits</h1>
-      <p className="modal-item">
-        <b>&#187; </b>Programming by{' '}
-        <a href="https://github.com/TheHappyKoala/" target="blank">
-          Darrell A. Huffman
-        </a>{' '}
-        , maintainer.
-      </p>
-      <p className="modal-item">
-        &#187; Development of the Verlet, RKF, PEFRL, and RKN family of
-        integrators by{' '}
-        <a href="https://hugogranstrom.com/" target="blank">
-          Hugo Granström
-        </a>, collaborator.
-      </p>
-      <p className="modal-item">
-        <b>&#187; </b>
-        <a
-          href="http://www.feynmanlectures.caltech.edu/I_toc.html"
-          target="blank"
-        >
-          Volume One of the Feynman Lectures on Physics
-        </a>{' '}
-        helped me wrap my head around the gravitational n-body problem and
-        Newtonian mechanics.
-      </p>
-      <p className="modal-item">
-        <b>&#187; </b>
-        <a href="https://nasa3d.arc.nasa.gov/models" target="blank">
-          Spacecraft 3D models
-        </a>{' '}
-        courtesy of NASA.
-      </p>
-      <p className="modal-item">
-        <b>&#187; </b>
-        <a
-          href="https://discourse.threejs.org/t/impact-shock-wave/5988"
-          target="blank"
-        >
-          Impact shockwave shader
-        </a>{' '}
-        developed by the ever spectacular and gloriously helpful prisoner849.
-      </p>
-      <p className="modal-item">
-        <b>&#187; </b>
-        Planet and moon textures courtesy of the talented{' '}
-        <a href="https://github.com/JohnVV" target="blank">
-          John Van Vlliet
-        </a>{' '}
-        and the dazzling{' '}
-        <a href="https://www.deviantart.com/magentameteorite" target="blank">
-          MagentaMeteorite (Brie Limburger)
-        </a>.
-      </p>
-      <p className="modal-item">
-        <b>&#187; </b>
-        Perlin noise for the star shaders by Stefan Gustavsson and Ashima Arts
-      </p>
-      <p className="modal-item">
-        <b>&#187; </b>
-        <a href="http://openuniverse.sourceforge.net/" target="blank">
-          Phobos and Deimos 3D models
-        </a>{' '}
-        courtesy of Raul Alvarez.
-      </p>
-      <p className="modal-item">
-        <b>&#187; </b>
-        State Vectors for solar system bodies were obtained from{' '}
-        <a href="https://ssd.jpl.nasa.gov/horizons.cgi" target="blank">
-          NASA JPL
-        </a>.
-      </p>
-      <p className="modal-item">
-        <b>&#187; </b>
-        The kind folks at{' '}
-        <a href="https://space.stackexchange.com/" target="blank">
-          space.stackexchange.com
-        </a>{' '}
-        and{' '}
-        <a href="https://scicomp.stackexchange.com/" target="blank">
-          scicomp.stackexchange.com
-        </a>{' '}
-        for kindly providing answers and feedback that helped me in the
-        development of this application.
-      </p>
-      <p className="modal-item">
-        <b>&#187; </b>
-        <a href="https://wikipedia.com/" target="blank">
-          WikiPedia
-        </a>{' '}
-        and all the awesome people that contribute to it with content and the
-        financial resources that keep it up and running.
-      </p>
-      <p className="modal-item">
-        <b>&#187; </b>
-        Mr. Doob and all the other people that created the{' '}
-        <a href="https://threejs.org/" target="blank">
-          THREE.js WebGL library
-        </a>.
-      </p>
-      <p className="modal-item">
-        <b>&#187; </b>
-        Last, but not least, I would like to credit the Universe for being
-        awesomely weird and thought provoking!
-      </p>
-    </Fragment>
-  );
-}
+export default () => (
+  <Fragment>
+    <h1>Credits</h1>
+    <p className="modal-item">
+      <b>&#187; </b>Programming by{' '}
+      <a href="https://github.com/TheHappyKoala/" target="blank">
+        Darrell A. Huffman
+      </a>{' '}
+      , maintainer.
+    </p>
+    <p className="modal-item">
+      &#187; Development of the Verlet, RKF, PEFRL, and RKN family of
+      integrators by{' '}
+      <a href="https://hugogranstrom.com/" target="blank">
+        Hugo Granström
+      </a>, collaborator.
+    </p>
+    <p className="modal-item">
+      <b>&#187; </b>
+      <a
+        href="http://www.feynmanlectures.caltech.edu/I_toc.html"
+        target="blank"
+      >
+        Volume One of the Feynman Lectures on Physics
+      </a>{' '}
+      helped me wrap my head around the gravitational n-body problem and
+      Newtonian mechanics.
+    </p>
+    <p className="modal-item">
+      <b>&#187; </b>
+      <a href="https://nasa3d.arc.nasa.gov/models" target="blank">
+        Spacecraft 3D models
+      </a>{' '}
+      courtesy of NASA.
+    </p>
+    <p className="modal-item">
+      <b>&#187; </b>
+      <a
+        href="https://discourse.threejs.org/t/impact-shock-wave/5988"
+        target="blank"
+      >
+        Impact shockwave shader
+      </a>{' '}
+      developed by the ever spectacular and gloriously helpful prisoner849.
+    </p>
+    <p className="modal-item">
+      <b>&#187; </b>
+      Planet and moon textures courtesy of the talented{' '}
+      <a href="https://github.com/JohnVV" target="blank">
+        John Van Vlliet
+      </a>{' '}
+      and the dazzling{' '}
+      <a href="https://www.deviantart.com/magentameteorite" target="blank">
+        MagentaMeteorite (Brie Limburger)
+      </a>.
+    </p>
+    <p className="modal-item">
+      <b>&#187; </b>
+      Perlin noise for the star shaders by Stefan Gustavsson and Ashima Arts
+    </p>
+    <p className="modal-item">
+      <b>&#187; </b>
+      <a href="http://openuniverse.sourceforge.net/" target="blank">
+        Phobos and Deimos 3D models
+      </a>{' '}
+      courtesy of Raul Alvarez.
+    </p>
+    <p className="modal-item">
+      <b>&#187; </b>
+      State Vectors for solar system bodies were obtained from{' '}
+      <a href="https://ssd.jpl.nasa.gov/horizons.cgi" target="blank">
+        NASA JPL
+      </a>.
+    </p>
+    <p className="modal-item">
+      <b>&#187; </b>
+      The kind folks at{' '}
+      <a href="https://space.stackexchange.com/" target="blank">
+        space.stackexchange.com
+      </a>{' '}
+      and{' '}
+      <a href="https://scicomp.stackexchange.com/" target="blank">
+        scicomp.stackexchange.com
+      </a>{' '}
+      for kindly providing answers and feedback that helped me in the
+      development of this application.
+    </p>
+    <p className="modal-item">
+      <b>&#187; </b>
+      <a href="https://wikipedia.com/" target="blank">
+        WikiPedia
+      </a>{' '}
+      and all the awesome people that contribute to it with content and the
+      financial resources that keep it up and running.
+    </p>
+    <p className="modal-item">
+      <b>&#187; </b>
+      Mr. Doob and all the other people that created the{' '}
+      <a href="https://threejs.org/" target="blank">
+        THREE.js WebGL library
+      </a>.
+    </p>
+    <p className="modal-item">
+      <b>&#187; </b>
+      Last, but not least, I would like to credit the Universe for being
+      awesomely weird and thought provoking!
+    </p>
+  </Fragment>
+);
