a8181133d94d8918a67669551d4b20df709d4ddc
Darrell <rather.glorious@gmail.com>
2019-04-21 20:03:45 +0200

    Refactor LazyDog component

--- src/js/components/LazyDog/index.js
+++ src/js/components/LazyDog/index.js
@@ -1,33 +1,22 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import './LazyDog.less';
 
-export default class extends Component {
-  constructor(props) {
-    super(props);
+export default props => {
+  const [loading, setLoading] = useState(true);
 
-    this.state = {
-      imageLoading: true
-    };
-  }
-
-  handleOnLoad = () => this.setState({ imageLoading: false });
-
-  render() {
-    return (
-      <figure className="lazy-dog-wrapper">
-        <img
-          onLoad={this.handleOnLoad}
-          src={this.props.src}
-          alt={this.props.alt}
-          width={this.props.width}
-          height={this.props.height}
-          className={!this.state.imageLoading && 'loaded-image'}
-        />
-        <figcaption>{this.props.caption}</figcaption>
-        {this.state.imageLoading && (
-          <i className={this.props.placeHolderIcon} />
-        )}
-      </figure>
-    );
-  }
-}
+  return (
+    <figure className="lazy-dog-wrapper">
+      <img
+        onLoad={() => setLoading(false)}
+        src={props.src}
+        alt={props.alt}
+        width={props.width}
+        height={props.height}
+        className={!loading && 'loaded-image'}
+      />
+      <figcaption>{props.caption}</figcaption>
+      {loading && <i className={props.placeHolderIcon} />}
+    </figure>
+  );
+};
+        
