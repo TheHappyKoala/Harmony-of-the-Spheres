3ff1f412155cdf283e972c1cc27bcc1e47fba955
Darrell <rather.glorious@gmail.com>
2019-07-06 23:02:19 +0200

    [Version 0.1.43][Build]

--- dist/components/Button/index.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import './Button.less';
-export default ({ callback, children, cssClassName }) => {
-    return (React.createElement("div", { className: cssClassName, onClick: callback }, children));
-};
-//# sourceMappingURL=index.js.map
--- dist/components/Modal/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import './Modal.less';
-export default ({ children, callback }) => (React.createElement("div", { className: "modal-wrapper" },
-    React.createElement("section", { className: "modal" },
-        children,
-        React.createElement("button", { onClick: callback, className: "modal-close-button" }, "X"))));
-//# sourceMappingURL=index.js.map
