374ee2eab58a2093e0df7122dc068bb177cc396e
Darrell <rather.glorious@gmail.com>
2019-07-06 23:02:19 +0200

    [Version 0.1.43][Build]

--- components/Tweet/index.js
+++
@@ -1,4 +0,0 @@
-import React, { memo } from 'react';
-export default memo(({ shareText, shareUrl, callToAction, cssClassName, hashtags }) => (React.createElement("a", { target: "_blank", rel: "noopener noreferrer", href: `https://twitter.com/intent/tweet/?text=${encodeURI(shareText)}&url=${encodeURIComponent(shareUrl)}&hashtags=${hashtags}`, className: cssClassName },
-    React.createElement("span", null, callToAction))), (prevProps, nextProps) => prevProps.shareUrl === nextProps.shareUrl);
-//# sourceMappingURL=index.js.map
--- reducers/index.js
+++
@@ -1,4 +0,0 @@
-import { combineReducers } from 'redux';
-import scenario from './scenario';
-export const rootReducer = combineReducers({ scenario });
-//# sourceMappingURL=index.js.map
