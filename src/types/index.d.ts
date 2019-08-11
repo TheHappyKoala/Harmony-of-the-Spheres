6185a6ccc36b7c2aeabf9c84ffc1e2f89f549f3a
Darrell <rather.glorious@gmail.com>
2019-08-11 17:10:38 +0200

    Declare type for the TrajectoryCruncher Web-Worker
    Otherwise TS won't know what the hell we're up to...

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -2,3 +2,10 @@ declare module 'react-redux';
 declare module 'react-addons-css-transition-group';
 declare const DEFAULT_SCENARIO: string;
 declare const EXOPLANET_ARCHIVE_DATA: { query: string; alias: string }[];
+declare module 'worker-loader!../../Physics/spacecraft/trajectoryCruncher' {
+  class WebpackWorker extends Worker {
+    constructor();
+  }
+
+  export default WebpackWorker;
+}
