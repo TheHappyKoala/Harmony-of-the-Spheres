f8437cebd9dfd3665723acb8326a788cb4e5cb67
Darrell <rather.glorious@gmail.com>
2019-08-11 17:00:07 +0200

    Add ts types for the Webpack DefinePlugin variables that we set

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -1,2 +1,4 @@
 declare module 'react-redux';
 declare module 'react-addons-css-transition-group';
+declare const DEFAULT_SCENARIO: string;
+declare const EXOPLANET_ARCHIVE_DATA: { query: string; alias: string }[];
