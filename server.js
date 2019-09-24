48d9d11625a09d9f3bb79c02949126a41bd3fc8b
Darrell <rather.glorious@gmail.com>
2019-09-24 19:00:42 +0200

    feat: add half-baked static file server for fetching json files locally

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
+app.use(cors());
+
+app.use(express.static(`${__dirname}/src/data/scenarios/`));
+
+const port = 9000;
+
+app.listen(port, () => console.log(`Listening very intently to ${port}`));
