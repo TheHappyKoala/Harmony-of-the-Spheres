fcbee87ce8a8e5f34a2b3e85f90db0090c20c525
Darrell <rather.glorious@gmail.com>
2019-09-24 22:52:52 +0200

    feat: add script for aggregating scenario meta data

---
+++ listScenarios.js
@@ -0,0 +1,30 @@
+const path = require(`path`);
+const fs = require("fs");
+
+const extractJSON = file =>
+  JSON.parse(
+    fs.readFileSync(file, {
+      encoding: "utf-8"
+    })
+  );
+
+const hooverDir = dir => {
+  const data = [];
+  fs.readdirSync(dir).forEach(file => {
+    if (path.extname(file) === ".json")
+      data.push({fileName: file, ...extractJSON(dir.concat(file))});
+  });
+
+  return data;
+};
+
+fs.writeFileSync(
+  "./src/data/list.json",
+  JSON.stringify(
+    hooverDir("./src/data/scenarios/").map(scenario => ({
+      name: scenario.name,
+      type: scenario.type,
+      fileName: scenario.fileName
+    }))
+  )
+);
