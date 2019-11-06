f6d7b25c9b89d8dd2fff1d4155526129548b8406
Darrell <rather.glorious@gmail.com>
2019-11-06 12:00:45 +0100

    add less support

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,6 +7,7 @@ module.exports = {
   plugins: [
     `gatsby-plugin-typescript`,
     `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-less`,
     `gatsby-plugin-sharp`,
     `gatsby-transformer-sharp`,
     `gatsby-transformer-json`,
