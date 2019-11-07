6580c8da70897a992b6d32fdf3d33976f370a3ae
Darrell <rather.glorious@gmail.com>
2019-11-07 20:19:34 +0100

    add scenario links

--- src/components/Navigation/index.tsx
+++ src/components/Navigation/index.tsx
@@ -1,4 +1,5 @@
 import React, { ReactElement, Fragment, useState } from "react";
+import { Link } from "gatsby";
 
 interface NavigationProps {
   scenariosInCategory: { node: { name: string } }[];
@@ -8,7 +9,9 @@ export default ({ scenariosInCategory }: NavigationProps): ReactElement => {
   return (
     <div className="filter-bar">
       {scenariosInCategory.map(({ node }) => (
-        <div>{node.name}</div>
+        <Link to={`/${node.name}`}>
+          <div>{node.name}</div>
+        </Link>
       ))}
     </div>
   );
