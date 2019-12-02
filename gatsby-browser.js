edef9d58d926cbe9020c7ab0e25ce056c146e6c8
Darrell <rather.glorious@gmail.com>
2019-12-02 20:35:03 +0100

    check for null, not false

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,7 +1,7 @@
 import wrapWithProvider from "./wrap-with-provider";
 
 export const onRouteUpdate = ({ prevLocation }) => {
-  window.PREVIOUS_PATH = prevLocation ? prevLocation.pathname : false;
+  window.PREVIOUS_PATH = prevLocation ? prevLocation.pathname : null;
 };
 
 export const wrapRootElement = wrapWithProvider;
