03425d0418f02426383144ff5bdc0f59cf3bde1e
Darrell <rather.glorious@gmail.com>
2019-12-04 21:42:07 +0100

    add header component

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,54 @@
+import React, { ReactElement, Fragment } from "react";
+import { Link } from "gatsby";
+import Nav from "../Nav";
+import NavItem from "../NavItem";
+import Button from "../Button";
+import Head from "../Head";
+import "./Header.less";
+
+interface HeaderProps {
+  pageTitle: string;
+  pageDescription: string;
+  location: any;
+}
+
+export default ({
+  pageTitle,
+  pageDescription,
+  location
+}: HeaderProps): ReactElement => (
+  <Fragment>
+    <Head pageTitle={pageTitle} pageDescription={pageDescription} />
+    <header>
+      <Link to="/">
+        <h1>Gravity Simulator</h1>
+      </Link>
+      <Nav css={{ border: "none", padding: 0, backgroundColor: "transparent" }}>
+        <NavItem active={location.pathname === "/credits"}>
+          <Link to="/credits">
+            {" "}
+            <Button cssClassName="button">
+              <span>
+                <i className="fas fa-glass fa-2x button" />
+                Credits
+              </span>
+            </Button>
+          </Link>
+        </NavItem>
+        <NavItem>
+          <Button cssClassName="button">
+            <a
+              href="https://github.com/TheHappyKoala/Harmony-of-the-Spheres"
+              target="blank"
+            >
+              <span>
+                <i className="fas fa-github fa-2x" />
+                Contribute
+              </span>
+            </a>
+          </Button>
+        </NavItem>
+      </Nav>
+    </header>
+  </Fragment>
+);
