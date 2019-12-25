2f133059daaa2ca8f7d090d395f8a58e0d7849fd
Darrell <rather.glorious@gmail.com>
2019-12-25 13:21:59 +0100

    set appropriate tol, min and max dt values for each scenario

--- src/physics/utils.js
+++ src/physics/utils.js
@@ -128,9 +128,9 @@ export function degreesToRadians(degrees) {
 export const radiansToDegrees = radians => radians * 57.295779513;
 
 export const calculateOrbitalVertices = (orbitalPeriod, dt) => {
-  const maxVertices = 200000;
+  const maxVertices = 30000;
   const orbitalVertices = parseFloat(
-    ((orbitalPeriod / (dt * 3)) * 1.1).toFixed(0)
+    (orbitalPeriod / dt * 1.1).toFixed(0)
   );
 
   return orbitalVertices > maxVertices || isNaN(orbitalVertices)
