44c2c9f41dd967d83f5ad37c3f7252c710325ea4
Darrell Huffman <rather.glorious@gmail.com>
2020-11-08 20:28:57 +0100

    fix build errors

--- src/templates/HallOfFamePlanetsNavigation.tsx
+++ src/templates/HallOfFamePlanetsNavigation.tsx
@@ -122,7 +122,7 @@ export default ({ data, pageContext, location }: IndexProps): ReactElement => {
                 </NavItem>
               </Link>
 
-              {["Transit", "Radial Velocity", "Imaging"].map(
+              {["Transit", "Radial Velocity", "Imaging", "Microlensing"].map(
                 discoveryFacility => (
                   <Link to={`/exoplanets/${kebabCase(discoveryFacility)}`}>
                     <NavItem
