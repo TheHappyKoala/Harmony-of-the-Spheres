69388aacdd0f270f26b9d2d73cabf5159ad86f26
Darrell Huffman <rather.glorious@gmail.com>
2022-01-09 20:36:09 +0100

    add data on stellar mass radius relationship

---
+++ exoplanet-archive/stellarMassRadiusData.js
@@ -0,0 +1,18 @@
+module.exports = [
+  { mass: 40, radius: 18 },
+  { mass: 18, radius: 7.4 },
+  { mass: 6.5, radius: 3.8 },
+  { mass: 3.2, radius: 2.5 },
+  { mass: 2.1, radius: 1.7 },
+  { mass: 1.7, radius: 1.3 },
+  { mass: 1.3, radius: 1.2 },
+  { mass: 1.1, radius: 1.05 },
+  { mass: 1, radius: 1 },
+  { mass: 0.93, radius: 0.93 },
+  { mass: 0.78, radius: 0.85 },
+  { mass: 0.69, radius: 0.74 },
+  { mass: 0.6, radius: 0.51 },
+  { mass: 0.21, radius: 0.32 },
+  { mass: 0.1, radius: 0.13 },
+  { mass: 0.0898, radius: 0.1192 }
+];
