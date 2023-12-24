5ae973c9b091b14b5f72658b423a2db737455f8e
Darrell Huffman <rather.glorious@gmail.com>
2023-12-24 21:53:54 +0100

    feat: add method for diffing integrator with manifestation masses

--- src/scene/manifestations/index.ts
+++ src/scene/manifestations/index.ts
@@ -28,6 +28,7 @@ class ManifestationManager {
     switch (mass.type) {
       case "star":
         return new Star(mass, this.textureLoader).createManifestation();
+
       default:
         return new Manifestation(
           mass,
@@ -43,6 +44,30 @@ class ManifestationManager {
       this.manifestations.push(manifestation);
       this.scene.add(manifestation.object3D);
     });
+  }
+
+  public diff(updatedMasses: ScenarioMassesType) {
+    let i = 0;
+
+    while (i < this.manifestations.length) {
+      let entry1 = this.manifestations[i];
+
+      if (
+        updatedMasses.some(
+          (entry2: ScenarioMassType) => entry1!.mass.name === entry2.name,
+        )
+      )
+        ++i;
+      else {
+        const massToBeDeleted = this.scene.getObjectByName(
+          this.manifestations[i]!.mass.name,
+        ) as THREE.Object3D;
+
+        this.scene.remove(massToBeDeleted);
+
+        this.manifestations.splice(i, 1);
+      }
+    }
   }
 }
 
