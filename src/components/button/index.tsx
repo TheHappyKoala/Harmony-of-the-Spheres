d532047f9f3e835f8e24cab885b232d357ee3c5f
Darrell Huffman <rather.glorious@gmail.com>
2024-02-29 00:22:09 +0100

    feat: style button component

--- src/components/button/index.tsx
+++ src/components/button/index.tsx
@@ -1,12 +1,16 @@
-import React, { ReactElement, ReactNode, MouseEvent } from "react";
+import React, { ReactNode, MouseEvent } from "react";
+
+import { button } from "./button.module.css";
 
 type Props = {
   callback: (event: MouseEvent<HTMLDivElement>) => void;
   children: ReactNode;
 };
 
-const Button = ({ callback, children }: Props): ReactElement => (
-  <div onClick={callback}>{children}</div>
+const Button = ({ callback, children }: Props) => (
+  <div className={button} onClick={callback}>
+    {children}
+  </div>
 );
 
 export default Button;
